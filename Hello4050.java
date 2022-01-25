03d28940d2a5fb5c1797b135678683a5ab987541
swagashek1 <am93029@uga.edu>
2022-01-25 01:42:55 -0500

    init

---
+++ Hello4050.java
@@ -0,0 +1,8 @@
+public class Hello4050 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello 4050");
+    }
+
+}
