95bd0508806c97fcba941e6a4d765a0d50f0a349
swagashek1 <am93029@uga.edu>
2022-01-25 01:45:23 -0500

    git commit -m "put in 10 hellos"

--- Hello4050.java
+++ Hello4050.java
@@ -3,6 +3,9 @@ public class Hello4050 {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello 4050");
+        for(int i=0; i<10; i++){
+            System.out.println("hello");
+        }
     }
 
 }
